2ea8ccd1ea0b8e32ce32175f0a904b0f2a70d9bf
Obafemi <81351455+timfemey@users.noreply.github.com>
2022-10-31 14:34:38 +0100

    patch: No. of Images Sent to Client Increase

--- server/helpers/seeImgs/mediaShow.go
+++ server/helpers/seeImgs/mediaShow.go
@@ -9,26 +9,24 @@ import (
 	"github.com/imagekit-developer/imagekit-go/api/media"
 )
 
-	var privateKey = config.EnvPrivateAPISecret()
-	var publicKey = config.EnvPublicAPIKey()
-	var urlEndpoint = config.EnvURL()
+var privateKey = config.EnvPrivateAPISecret()
+var publicKey = config.EnvPublicAPIKey()
+var urlEndpoint = config.EnvURL()
 
-	//Initialize ImageKit Instance and Pass environment variables
-	var ik = ImageKit.NewFromParams(ImageKit.NewParams{
-		PrivateKey:  privateKey,
-		PublicKey:   publicKey,
-		UrlEndpoint: urlEndpoint,
-	})
+// Initialize ImageKit Instance and Pass environment variables
+var ik = ImageKit.NewFromParams(ImageKit.NewParams{
+	PrivateKey:  privateKey,
+	PublicKey:   publicKey,
+	UrlEndpoint: urlEndpoint,
+})
 
 func SeeImages() ([]media.File, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-
 	resp, err := ik.Media.Files(ctx, media.FilesParam{
-		Skip:        0,
-		Limit:       35,
-		
+		Skip:  0,
+		Limit: 50,
 	})
 	if err != nil {
 		return nil, err
